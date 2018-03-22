91eced707a218beb65be5f4b11d1af675d390d23
a-man010 <dl2s6871>
2018-03-22 23:51:42 +0530

    Fetching data using CoreML

--- NotHotDog/ViewController.swift
+++ NotHotDog/ViewController.swift
@@ -7,19 +7,86 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imagedisplay: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.sourceType = .camera
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let userPickedimage = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            
+            imagedisplay.image = userPickedimage
+            
+            imagePicker.dismiss(animated: true, completion: nil)
+            
+            guard let ciimage = CIImage(image : userPickedimage) else {
+                fatalError("Could'nt convert to CIImage")
+            }
+            detect(image: ciimage)
+        }
+        
     }
-
-
+    
+    func detect(image : CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else{
+            fatalError("Loading CoreML model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("VNCoreMLRequest Failed")
+            }
+            
+            if let firstResult = results.first{
+                print("\(firstResult.identifier)!")
+                DispatchQueue.main.async {
+                    self.navigationItem.title = String(firstResult.identifier)
+                }
+                
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage : image)
+        
+        do{
+            try handler.perform([request])
+            
+        }catch{
+            print(error)
+        }
+        
+        
+    }
+    
+    
+    
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    
+    
+    
 }
+
 
